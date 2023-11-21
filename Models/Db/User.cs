15f18ed043f60309146f9109a6cb5d6458975eb1
revad888 <revad8@gmail.com>
2023-11-21 21:48:46 +0300

    Initial commit

---
+++ Models/Db/User.cs
@@ -0,0 +1,10 @@
+namespace Module32.Models.Db;
+
+public class User
+{
+    // Уникальный идентификатор сущности в базе
+    public Guid Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public DateTime JoinDate { get; set; }
+}
---
+++ Models/Db/UserPost.cs
@@ -0,0 +1,11 @@
+namespace Module32.Models.Db;
+
+public class UserPost
+{
+    public Guid Id { get; set; }
+    public DateTime Date { get; set; }
+    public string Title { get; set; }
+    public string Text { get; set; }
+    public Guid UserId { get; set; }
+    public User User { get; set; }
+}
